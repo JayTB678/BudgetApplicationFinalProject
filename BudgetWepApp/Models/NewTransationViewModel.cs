711f9e220fff580f34b1fa51ad3ffb1402caf907
ImagineX12 <86370767+ImagineX12@users.noreply.github.com>
2025-03-18 22:02:38 -0500

    Added way to add incomes

---
+++ BudgetWepApp/Models/NewTransationViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BudgetWepApp.Models
+{
+    public class NewTransationViewModel
+    {
+        public double Amount { get; set; }
+
+        //leave null if its not reacuring
+        public int? FrequencyInDays { get; set; }
+
+        public bool UpdateTransactions { get; set; }
+    }
+}
