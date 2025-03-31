0faf0dcc0c18ab9cd48ce1ef99e83729df5eacc3
ptCady1 <ptCady0@gmail.com>
2025-03-30 19:07:05 -0500

    adding Login and Signin background

---
+++ BudgetWepApp/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+
+namespace BudgetWepApp.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Please enter username")]
+        public string UserName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Please enter email")]
+        public string Email { get; set; } = string.Empty ;
+        [Required(ErrorMessage = "Please enter password")]
+        public string Password { get; set; } = string.Empty;
+        public string returnUrl { get; set; } = string.Empty;
+        public bool rememberMe { get; set; }
+    }
+}
