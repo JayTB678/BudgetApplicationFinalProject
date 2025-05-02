9009c884c8320c21c915e86e7d65b794435f6bc2
ptCady1 <ptCady0@gmail.com>
2025-05-01 22:15:34 -0500

    component

---
+++ BudgetWepApp/Views/Shared/Components/GoalLink/Default.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BudgetWepApp.Models;
+namespace BudgetWepApp.Views.Shared.Components.GoalLink
+{
+    public class Default : ViewComponent
+    {
+        public IViewComponentResult Invoke(string name)
+        {
+            Goal goal = new Goal
+            {
+               Name = name,
+            };
+            return View(goal);
+        }
+
+    }
+}
